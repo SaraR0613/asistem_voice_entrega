a1d1a42ce7df9b95a558d1e522f183b9eb7c1bb3
Kevin Gomez <gomezdiezkevinmateo@gmail.com>
2024-09-18 20:54:07 -0500

    Update Class

--- Model/model2.py
+++ Model/model2.py
@@ -2,35 +2,45 @@ import pyttsx3
 import os
 import datetime
 
-
 # Inicializa el motor de texto a voz
 engine = pyttsx3.init()
 
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
 
-def open_file(file_path):
-    os.system(f'start {file_path}')
+class SpeechRecognitionSystem:
 
-def open_google_tab():
-    url = 'https://www.google.com'
-    os.system(f'start {url}')
+    def speak(self, text_: str = "buenas"):
+        pyttsx3.init().say(text_)
+        pyttsx3.init().runAndWait()
 
-def get_datetime():
-    now = datetime.datetime.now()
-    date_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    speak(f"La fecha y hora actual son: {date_time_str}")
+    def get_datetime(self):
+        now = datetime.datetime.now()
+        date_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+        pyttsx3.speak(f"La fecha y hora actual son: {date_time_str}")
 
-def open_notepad():
-    os.system('notepad')
 
-#engine.say("por favor ingrese un texto")
-#engine.runAndWait()
-#a = input("por favor ingrese un texto: ")
-#b = speak(a)
-#c = open_file("Nuevo")
-#e = open_google_tab()
-#f = open_notepad()
-#g = open_file("C:\Prueba")
-#h = get_datetime()
+class Fillemanager:
+
+    def open_file(self, file_path: str):
+        os.system(f'start {file_path}')
+
+    def open_notepad(self):
+        os.system('notepad')
+
+
+class Webnavegartor:
+
+    def open_google_tab(self):
+        url = 'https://www.google.com'
+        os.system(f'start {url}')
+
+
+engine.say("por favor ingrese un texto")
+engine.runAndWait()
+a = input("por favor ingrese un texto: ")
+b = SpeechRecognitionSystem().speak(a)
+c = SpeechRecognitionSystem().get_datetime()
+d = Fillemanager().open_file("C:\Prueba")
+e = Fillemanager().open_notepad()
+f = Webnavegartor().open_google_tab()
+
+
